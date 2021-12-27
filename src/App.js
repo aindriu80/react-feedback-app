15c685ea89b20129926cb924fdce993d1a4a1fcb
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2021-12-27 11:29:21 +0000

    Closing feedbacks - Handling events from App.js with prop drilling

--- src/App.js
+++ src/App.js
@@ -5,12 +5,17 @@ import FeedbackData from './data/FeedbackData'
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData)
+  const handleDelete = (id) => {
+    if (window.confirm('Are you sure you want to delete this feedback?')) {
+      setFeedback(feedback.filter((item) => item.id !== id))
+    }
+  }
 
   return (
     <>
       <Header />
       <div className="container">
-        <FeedbackList feedback={feedback} />
+        <FeedbackList feedback={feedback} handleDelete={handleDelete} />
       </div>
     </>
   )
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,8 +1,7 @@
 import PropTypes from 'prop-types'
 import FeedbackItem from './FeedbackItem'
-import React from 'react'
 
-const FeedbackList = ({ feedback }) => {
+const FeedbackList = ({ feedback, handleDelete }) => {
   if (!feedback || feedback.length === 0) {
     return <p>No feedback yet</p>
   }
@@ -10,7 +9,7 @@ const FeedbackList = ({ feedback }) => {
   return (
     <div className="feedback-list">
       {feedback.map((item) => (
-        <FeedbackItem key={item.id} item={item} />
+        <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
       ))}
     </div>
   )
