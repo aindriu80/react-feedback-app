d3c137ac8f3201684fb9f1a89cadb60f8ea3b4cc
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2021-12-27 11:21:24 +0000

    Events initial

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,9 +1,17 @@
+import { FaTimes } from 'react-icons/fa'
 import PropTypes from 'prop-types'
 import Card from './shared/Card'
+
 function FeedbackItem({ item }) {
+  const handleClick = (id) => {
+    console.log('Item ID:', id)
+  }
   return (
     <Card reverse={true}>
       <div className="num-display">{item.rating}</div>
+      <button onClick={() => handleClick(item.id)} className="close">
+        <FaTimes color="purple" />
+      </button>
       <div className="text-display">{item.text}</div>
     </Card>
   )
