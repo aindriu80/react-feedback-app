1e624a0b6e07285db5292088f8758488f23e86b0
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2021-12-29 10:37:07 +0000

    Navlink & useParams

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import { v4 as uuidv4 } from 'uuid'
 import { useState } from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  NavLink,
+} from 'react-router-dom'
+import Card from './components/shared/Card'
 import Header from './components/Header'
 import FeedbackList from './components/FeedbackList'
 import FeedbackData from './data/FeedbackData'
@@ -8,6 +14,7 @@ import FeedbackStats from './components/FeedbackStats'
 import FeedbackForm from './components/FeedbackForm'
 import AboutPage from './pages/AboutPage'
 import AboutIconLink from './components/AboutIconLink'
+import Post from './components/Post'
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData)
@@ -42,7 +49,16 @@ function App() {
               </>
             }></Route>
           <Route path="/about" element={<AboutPage />} />
+          <Route path="/post/:id/:name" element={<Post />} />
         </Routes>
+        <Card>
+          <NavLink to="/" activeClassName="active">
+            Home
+          </NavLink>
+          <NavLink to="/about" activeClassName="active">
+            about
+          </NavLink>
+        </Card>
         <AboutIconLink />
       </div>
     </Router>
--- src/components/AboutIconLink.jsx
+++ src/components/AboutIconLink.jsx
@@ -6,8 +6,6 @@ const AboutIconLink = () => {
     <div>
       <div className="about-link">
         <Link to="/about">
-          {/* Passing an object with pathname, query params like search and sort and a hash 'hello' */}
-          {/* <Link to={{ pathname: '/about', search: '?sort=name', hash: '#hello' }}> */}
           <FaQuestion size={30} />
         </Link>
       </div>
